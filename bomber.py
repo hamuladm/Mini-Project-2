8146ff71a9ae6bf54ebb5b994f22927b0ebebdd5
dmytro <hamula.dm@gmail.com>
2022-11-30 12:36:03 +0200

    Initial commit

--- bomber.py
+++
@@ -1,9 +0,0 @@
-import pyautogui as pg
-
-with open ('bomber.txt', 'w', encoding = 'utf-8') as f:
-    for _ in range (1000):
-        f.write('STAS LOX\n')
-with open ('bomber.txt', 'r', encoding = 'utf-8') as f1:
-    for line in f1:
-        pg.write(line)
-        pg.press('Enter')
